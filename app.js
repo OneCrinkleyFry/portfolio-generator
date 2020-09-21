6ebd790966d3369dbcae69e76e923e152b3bc400
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-09-21 11:55:13 -0600

    output the string for the html document

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2,process.argv.length);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);   
-    }
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-compatible" content="ie=edge">
+            <title>Portfolio Demo</title>
+        </head>
 
-    console.log("===========================");
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+        <body>
+            <h1>${name}</h1>
+            <h2><a href="https://github.com/${github}">Github</a></h2>
+        </body>
+    </html>
+    `;
+}
+console.log(name, github);
+console.log(generatePage(name, github));
